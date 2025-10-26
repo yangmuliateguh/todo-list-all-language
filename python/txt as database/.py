33d89af27185963e3443c83b176eb11ece70825d
yangmuliateguh <teguh.kahfiyasin@gmail.com>
2025-10-26 19:10:19 +0700

    developing create function and reader for the file

--- python/txt as database/.py
+++ python/txt as database/.py
@@ -2,13 +2,32 @@ from pathlib import Path
 
 path = Path.cwd() / 'python' / 'txt as database' / 'todo.txt'
 
+def todo_reader():
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            next(f)
+            for line in f:
+                if line.strip():
+
 def read_todo():
     try:
         with open(path, 'r', encoding='utf-8') as f:
+            next(f)
             for line in f:
-                id, todo, status = line.strip().split('|')
-                print(f'{id}. {todo} ({status})')
+                if line.strip():
+                    id, todo, status = line.strip().split('|')
+                    return [id, todo, status]
+            return ['file kosong']
+    except FileNotFoundError:
+        return ('file tidak ada')
+
+def create_todo():
+    todo = input('masukan todo : ')
+    try:
+        with open(path, 'a', encoding='utf-8') as f:
+            f.write(f'\n{id}|{todo}|belum')
     except FileNotFoundError:
         print('file tidak ada')
 
-read_todo()
+print(read_todo())
+# create_todo()
